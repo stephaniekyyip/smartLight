c33b444d5115e4dd94bea8dea2faa50b717be9e1
Stephanie Yip <stephaniekyyip@gmail.com>
2018-12-15 20:06:49 -1000

    Update brightnessAdjust4.py

--- Python/brightnessAdjust4.py
+++ Python/brightnessAdjust4.py
@@ -16,8 +16,8 @@ led_pin = 13
 
 #For LED
 GPIO.setup(led_pin, GPIO.OUT) #set pin 11 to output
-p = GPIO.PWM(led_pin,100) #set the PWM for LED to 50%
-p.start(100) #I don’t remember what this does but trust me, you need it.
+p = GPIO.PWM(led_pin,100) #set PWM output on led_pin to 100Hz
+p.start(100) #Start led_pin with 100% duty cycle 
 
 
 #Catch when script is interupted, cleanup correctly
