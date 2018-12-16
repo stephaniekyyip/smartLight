2d2a0c448f72d29f7e451e9af03a200a9105edb0
Stephanie Yip <stephaniekyyip@gmail.com>
2018-12-15 20:07:26 -1000

    Update photoresistor_w_led.py

--- Python/photoresistor_w_led.py
+++ Python/photoresistor_w_led.py
@@ -21,9 +21,9 @@ pin_to_circuit = 7 #to control photoresistor
 led_pin = 13
 
 #For LED
-GPIO.setup(led_pin, GPIO.OUT) #set pin 11 to output
-p = GPIO.PWM(led_pin,100) #set the PWM for LED to 50%
-p.start(100) #I don’t remember what this does but trust me, you need it.
+GPIO.setup(led_pin, GPIO.OUT) #set pin 13 to output
+p = GPIO.PWM(led_pin,100) #set PWM output on led_pin to 100Hz
+p.start(100) #Start led_pin with 0% duty cycle 
 
 def rc_time (pin_to_circuit):
     count = 0
